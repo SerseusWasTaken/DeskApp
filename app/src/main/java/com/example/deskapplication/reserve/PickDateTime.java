5ebb55e6c73869d1a939b6780e9708cee882946e
Jonas <jonas.schlecht16@gmail.com>
2023-03-21 18:20:10 +0100

    adds reusable header for app

---
+++ app/src/main/java/com/example/deskapplication/reserve/PickDateTime.java
@@ -0,0 +1,14 @@
+package com.example.deskapplication.reserve;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.deskapplication.R;
+
+public class PickDateTime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_date_time);
+    }
+}
