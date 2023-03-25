b5ffe3797bfbabd26cd0069903ba94ea00ae6891
Jonas <jonas.schlecht16@gmail.com>
2023-03-25 12:23:22 +0100

    adds activities and placeholder content

--- app/src/main/java/com/example/deskapplication/cancel/CancelDeskActivity.java
+++ app/src/main/java/com/example/deskapplication/cancel/CancelDeskActivity.java
@@ -2,7 +2,9 @@ package com.example.deskapplication.cancel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.deskapplication.R;
 
@@ -12,5 +14,8 @@ public class CancelDeskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cancel_desk);
+
+        Button cancelOwnDeskButton = findViewById(R.id.cancelOwnDeskButton);
+        cancelOwnDeskButton.setOnClickListener(view -> startActivity(new Intent(CancelDeskActivity.this, CancelDeskDetailActivity.class)));
     }
 }
--- app/src/main/java/com/example/deskapplication/reserve/PickDateTimeActivity.java
+++ app/src/main/java/com/example/deskapplication/reserve/PickDateTimeActivity.java
@@ -1,7 +1,11 @@
 package com.example.deskapplication.reserve;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import com.example.deskapplication.R;
 
 public class PickDateTimeActivity extends AppCompatActivity {
@@ -10,6 +14,9 @@ public class PickDateTimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_date_time);
+
+        Button chooseDeskButton = findViewById(R.id.chooseDeskButton);
+        chooseDeskButton.setOnClickListener(view -> startActivity(new Intent(PickDateTimeActivity.this, PickDeskActivity.class)));
 
     }
 }
